7aa58c7751ced14968a7bd43dab6abbec945c07e
Jess Chitas <chitasjessica@gmail.com>
2025-01-06 11:39:26 +0000

    potention build fix: add homepage to config
    Signed-off-by: Jess Chitas <chitasjessica@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  homepage:'https://github.com/containers/ramalama.ai.git',
   base: '/ramalama.ai/',
 })
