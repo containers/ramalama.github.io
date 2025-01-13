7c9f9d3f83e5f8584ea5d8844e0e5d2baa2ee27e
Jess Chitas <chitasjessica@gmail.com>
2025-01-13 12:52:16 +0000

    add base
    Signed-off-by: Jess Chitas <chitasjessica@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   homepage: "https://github.com/containers/ramalama.ai",
-  base: '/',
+  base: '/ramalama.ai/',
 })
