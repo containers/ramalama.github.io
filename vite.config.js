37930daf6ef69fef2945e54c2e08ecc811b73e58
Jess Chitas <chitasjessica@gmail.com>
2025-01-13 11:55:26 +0000

    prep for GitHub Actions
    Signed-off-by: Jess Chitas <chitasjessica@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  homepage:'https://github.com/containers/ramalama.ai.git',
-  base: '/ramalama.ai/',
+  base: '/',
 })
