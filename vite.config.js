5d00ff40f3e6f16a581b7680dfcd8e14c14142ab
Ian Eaves <ian.k.eaves@gmail.com>
2025-11-21 22:39:20 -0600

    prep for docs link
    Signed-off-by: Ian Eaves <ian.k.eaves@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,38 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import sirv from 'sirv'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const docsDir = resolve(__dirname, 'public/docs')
+
+const serveDocsPlugin = () => ({
+  name: 'serve-public-docs',
+  configureServer(server) {
+    const serve = sirv(docsDir, { dev: true })
+
+    server.middlewares.use((req, res, next) => {
+      const url = req.url
+      if (!url || !url.startsWith('/docs')) {
+        return next()
+      }
+
+      const parsedUrl = new URL(url, 'http://localhost')
+      const strippedPath = parsedUrl.pathname.slice('/docs'.length) || '/'
+      req.url = `${strippedPath}${parsedUrl.search}`
+
+      serve(req, res, (err) => {
+        req.url = url
+        next(err)
+      })
+    })
+  },
+})
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), serveDocsPlugin()],
   homepage: "https://github.com/containers/ramalama.github.io",
   base: '',
 })
